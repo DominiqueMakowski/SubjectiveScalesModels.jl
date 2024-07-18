960363905322f5f7f04393eac5055799be42a412
Dominique Makowski <dom.mak19@gmail.com>
2024-07-18 16:00:12 +0100

    remove doctest

--- src/BetaMuPhi.jl
+++ src/BetaMuPhi.jl
@@ -6,11 +6,7 @@ import Distributions: Beta
 
 Construct a Beta distribution with parameters mean `μ` and precision `ϕ`.
 
-# Examples
-```jldoctest
-julia> BetaMuPhi(0.5, 2)
-Beta{Float64}(α=1.0, β=1.0)
-```
+
 """
 function BetaMuPhi(μ::Number, ϕ::Number)
     return Beta(μ * ϕ, (1 - μ) * ϕ)
